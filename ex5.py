84a48a1adc89b3f882f9de970f39a434127d1a80
Scott Eissfeldt <scott@eissfeldt.com>
2018-10-18 20:06:04 -0700

    exercise 5 completed

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'BigChud McGraw'
+my_age = 43 # years
+my_height = 73 # inches
+my_weight = 185 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Blonde'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually, that's not too heavy.")
+print(f"He has {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
